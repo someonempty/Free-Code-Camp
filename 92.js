b4a4800c698bd7f6ae545c4c6adf214b49ccf639
someonempty <someonempty@gmail.com>
2022-08-22 15:20:03 +0300

    commit

---
+++ 92.js
@@ -0,0 +1,22 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }, {
+      "artist": "Billy",
+      "title": "Piano",
+      "release_year": 1977,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ]
+    }
+  
