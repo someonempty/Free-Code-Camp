955157998486bf76375b74ce7c64fc5b6dda92b0
someonempty <someonempty@gmail.com>
2022-08-22 15:14:03 +0300

    commit

---
+++ 91.js
@@ -0,0 +1,14 @@
+
+function checkObj(obj, checkProp) {
+    if (obj.hasOwnProperty(checkProp)) {
+      return obj[checkProp];
+    } else {
+      return "Not Found";
+    }
+  
+  
+    
+  
+    
+    // Only change code above this line
+  }
